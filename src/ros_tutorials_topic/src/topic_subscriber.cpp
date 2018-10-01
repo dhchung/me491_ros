d43ad1d3a9532cf6445c224c7f8b46f255ff1a4e
Dongha Chung <haetae.dongha@gmail.com>
2018-10-01 14:34:00 +0900

    adding files

---
+++ src/ros_tutorials_topic/src/topic_subscriber.cpp
@@ -0,0 +1,18 @@
+#include "ros/ros.h"
+#include "ros_tutorials_topic/MsgTutorial.h"
+
+void msgCallback(const ros_tutorials_topic::MsgTutorial::ConstPtr& msg)
+{
+ROS_INFO("recievemsg= %d", msg->stamp.sec);
+ROS_INFO("recievemsg= %d", msg->stamp.nsec);
+ROS_INFO("recievemsg= %d", msg->data);
+}
+
+int main(int argc, char **argv)
+{
+ros::init(argc, argv, "topic_subscriber");
+ros::NodeHandle nh;
+ros::Subscriber ros_tutorial_sub= nh.subscribe("ros_tutorial_msg", 100, msgCallback);
+ros::spin();
+return 0;
+}
